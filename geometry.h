18b33a036224eaa201e72a0ec287b2929b4a3d54
Bryan Gonzales <bronig11@gmail.com>
2017-04-21 10:16:22 -0600

    Added travis file

--- geometry.h
+++ geometry.h
@@ -8,6 +8,10 @@
  *
  */
 
+install make dep
+language c 
+script make && make test
+
 #include <stdbool.h>
 
 /* 2D Coordinate Struct */
@@ -24,3 +28,6 @@ bool coord_2d_eq(const coord_2d_t* a, const coord_2d_t* b);
 
 /* Calculate the midpoint between two 2D coordinates and load into mid */
 void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b);
+
+/* Take three 2d coordinates and return a double equal to the area of the triangle formed */
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c);
